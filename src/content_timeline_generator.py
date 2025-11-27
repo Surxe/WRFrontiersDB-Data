b8777d5334fcaeb341abd8a571dec8479d52bcb3
Surxe <119145352+Surxe@users.noreply.github.com>
2025-11-26 18:55:35 -0600

    Generate a full timeline of all additions to the game

---
+++ src/content_timeline_generator.py
@@ -0,0 +1,94 @@
+"""
+Create a timeline of all the new content that has released over time.
+
+TODO: Add it to the CI pipeline
+"""
+
+from datetime import datetime
+import os
+from loguru import logger
+from typing import TypedDict
+
+class SummaryEntry(TypedDict):
+    lines: list[str]
+
+class ContentTimelineGenerator:
+    def __init__(self, summaries_dir='summaries', lang_code='en'):
+        if lang_code not in ['en', 'id']:
+            raise ValueError("lang_code must be either 'en' or 'id'")
+        self.summaries_dir = summaries_dir
+        self.patch_dir = os.path.join(summaries_dir, 'patch')
+        self.timeline_dir = os.path.join(summaries_dir, 'timeline')
+        self.lang_code = lang_code
+        self.summaries = self._load_summaries()
+        self.timeline_lines = self._generate_timeline()
+        self._save_timeline(output_file=os.path.join(self.timeline_dir, f'content_timeline_{lang_code}.md'))
+
+    def _generate_timeline(self):
+        """
+        Generate a timeline of content additions from summaries.
+        Each entry contains the date, title, and description of the addition.
+        """
+        timeline_lines = ['# Added robots/pilots since the first public steam release']
+        for from_to_version, additions in self.summaries.items():
+            from_date, to_date = from_to_version.split('_to_')
+            if self.lang_code == 'id':
+                to_date = to_date
+            elif self.lang_code == 'en':
+                # 2025-11-15 -> November 15, 2025
+                to_date = datetime.strptime(to_date, "%Y-%m-%d").strftime("%B %d, %Y")
+            else:
+                raise ValueError("lang_code invalid")
+            
+            if not additions:
+                continue
+
+            timeline_lines.append(f"## {to_date}")
+            for addition in additions:
+                timeline_lines.append(f"**{addition}**")
+            timeline_lines.append("")  # Add a blank line for spacing
+               
+        logger.info(f"Generated timeline with {len(timeline_lines)} entries.")
+        return timeline_lines
+    
+    def _save_timeline(self, output_file:str):
+        """
+        Save the generated timeline to a markdown file.
+        """
+        if not output_file.endswith('.md'):
+            raise ValueError("output_file must be a markdown file with .md extension")
+        os.makedirs(os.path.dirname(output_file), exist_ok=True)
+        with open(output_file, 'w', encoding='utf-8') as f:
+            for line in self.timeline_lines:
+                f.write(line + '\n')
+        logger.info(f"Timeline saved to {output_file}.")
+    
+    def _load_summaries(self) -> SummaryEntry:
+        """
+        From summaries/patch/<from_to_version>/<lang_code>.md,
+        Save each line that starts with "* Added" into 
+        a SummaryEntry: {from_to_version: [lines]}
+        """
+        summaries = {}
+        for from_to_version in os.listdir(self.patch_dir): #from_to_version e.g. 2025-03-10_to_2025-03-13
+            subdir_path = os.path.join(self.patch_dir, from_to_version)
+            if not os.path.isdir(subdir_path):
+                continue # e.g. summaries/.gitkeep
+            file_to_read = os.path.join(subdir_path, f'{self.lang_code}.md')
+            if os.path.isfile(file_to_read):
+                additions = []
+                with open(file_to_read, 'r', encoding='utf-8') as f:
+                    for line in f:
+                        if line.startswith('* Added '):
+                            logger.debug(f"Found addition in {from_to_version}: {line.strip()}")
+                            additions.append(line.strip().removeprefix('* Added '))
+                if additions:
+                    summaries[from_to_version] = additions
+            else:
+                raise FileNotFoundError(f"Summary file not found: {file_to_read}")
+        logger.info(f"Loaded summaries for {len(summaries)} version(s).")
+        return summaries
+
+if __name__ == "__main__":
+    ContentTimelineGenerator(lang_code='en')
+    ContentTimelineGenerator(lang_code='id')
---
+++ summaries/timeline/content_timeline_en.md
@@ -0,0 +1,65 @@
+# Added robots/pilots since the first public steam release
+## April 17, 2025
+**Pilot Duncan Prasad**
+**Repulsor**
+**Scorpion**
+**Tusk**
+
+## June 17, 2025
+**Cascade**
+**Halo**
+**Loki**
+**Pilot Sora Sing**
+**Volta**
+
+## July 01, 2025
+**Jigsaw**
+
+## July 15, 2025
+**Bayonet**
+**Bisector**
+**Ceres**
+
+## July 29, 2025
+**Ammo Fabricator**
+**Locust**
+**Mesa**
+
+## August 12, 2025
+**Anansi**
+**MLx2**
+**Pilot Carmen Ruiz**
+**Scrubber**
+**Suppressor**
+
+## August 26, 2025
+**Nucleon**
+
+## September 09, 2025
+**Apollo**
+**Iron Veil**
+**Pilot "Dredge" Collins**
+**Pilot Emma James**
+**Pilot Giancarlo**
+**Volta**
+
+## September 23, 2025
+**Horde**
+**Kumo**
+
+## October 14, 2025
+**Kinetic Pulse**
+**Zenit**
+
+## October 28, 2025
+**Decker**
+**Hefty**
+**Pilot "Dredge" Collins**
+**Pilot Emma James**
+**Supply Surge**
+
+## November 11, 2025
+**Crix**
+**Pilot Giancarlo**
+**Stormpike**
+
